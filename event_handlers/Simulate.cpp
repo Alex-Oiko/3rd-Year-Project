d83f174f04de46e6c0d42df7ef6c462c0a7104ac
Alex-Oiko <alex.pana.oikonomou@gmail.com>
2013-02-06 11:04:32 +0000

    event handlers update

--- event_handlers/Simulate.cpp
+++ event_handlers/Simulate.cpp
@@ -15,18 +15,24 @@ void Simulate::Update(Event E,Core CORE,Deal DEAL){
 	else if(W==0){//multiplication
 		Vp=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].VinPoint;
 		temp=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value;
-		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=*CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];//which one????
+		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=*CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];//which one????
+		counter++;
 		LoadFire(Event E,Core CORE,Dealer DEAL);
 		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=temp;
 	}
 	else if(W==1){//addition
-		for(int node=0;node<Valency;node++){//Valecy care..how many and from where do you start??
+		for(int node=0;node<xNodes;node++){//Valecy care..how many and from where do you start??xNodes is the number of elements of the x vector
 			temp+=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];//replace with value or lasres???
 		}
+		counter++;
 		LoadFire(Event E,Core CORE,Dealer DEAL);
 
 	}
 	else if(W==2){//subtracting
+	
 
+	}
+	else if(W==3){//assignement
+		CORE.AllCores[Xd][Yd][Cd].Value=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];
 	}
 }
