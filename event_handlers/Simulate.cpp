bfde7e988f5fb7a9c2fefb34b673fd56b3fa70ac
Alex-Oiko <alex.pana.oikonomou@gmail.com>
2013-02-11 11:14:59 +0000

    event handlers almost finished. Check to see if it compiles

--- event_handlers/Simulate.cpp
+++ event_handlers/Simulate.cpp
@@ -15,14 +15,13 @@ void Simulate::Update(Event E,Core CORE,Deal DEAL){
 		//do nothing
 	}
 	else if(W==0){//multiplication
-		temp=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value;
-		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=*CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];//which one????
+		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].lasres=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value*CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];//which one????
+		E.Values=CORE.AllCores[Xd][Yd].TargetTable[Od].lasres;
 		LoadFire(Event E,Core CORE,Dealer DEAL);
-		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=temp;//not sure if this will work
 	}
 	else if(W==1){//addition
 		for(int node=0;node<xNodes;node++){//Valecy care..how many and from where do you start??xNodes is the number of elements of the x vector
-			temp+=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];//replace with value or lasres???
+			temp+=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];
 		}
 		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=temp;
 		LoadFire(Event E,Core CORE,Dealer DEAL);
@@ -38,7 +37,7 @@ void Simulate::Update(Event E,Core CORE,Deal DEAL){
 		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].lasres=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];
 	}
 	else if(W==4){//constants
-		for(int node=0;i<xNodes;i++){
+		for(int node=0;node<xNodes;node++){
 			AllCores[Xd][Yd][Cd].TargetTable[Od].Value+=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]]^2;
 		}
 			LoadFire(Event E,Core CORE,Deal DEAL);	
@@ -49,17 +48,18 @@ void Simulate::Update(Event E,Core CORE,Deal DEAL){
 			LoadFire(Event E,Core CORE,Deal DEAL);
 	}
 	else if(W==6){//calculate r & p'Ap
-		for(int node=0;node<xNodes;node++){//Valecy care..how many and from where do you start??xNodes is the number of elements of the x vector
-			temp+=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];//replace with value or lasres???
+		for(int node=0;node<xNodes;node++){//xNodes is the number of elements of the x vector
+			temp+=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];
 		}
-		if(from Ap){//think about this
-		CORE.AllCores[Xd][Yd][Cd].TargetTable.lasres=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value*CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];
-		E.Value=lasres;
-		LoadFire(Event E,Core CORE,Deal DEAL);
+		if(from Ap){//think about how you are going to distinguish between the 2 
+			CORE.AllCores[Xd][Yd][Cd].TargetTable.Values=temp;
+			CORE.AllCores[Xd][Yd][Cd].TargetTable.lasres=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value*temp;
+			E.Value=lasres;
+			LoadFire(Event E,Core CORE,Deal DEAL);
 		}
 		else{
-		CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value-CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];
-		LoadFire(Event E,Core CORE,Deal DEAL);
+			CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value-temp;
+			LoadFire(Event E,Core CORE,Deal DEAL);
 		}
 	}
 	else if(W==7){//division
@@ -77,10 +77,18 @@ void Simulate::Update(Event E,Core CORE,Deal DEAL){
 		E.Value=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od]=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value+CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[0]];
 		LoadFire(Event E,Core CORE,Deal DEAL);	
 	}
-
+	else if(w==11){
+		for(int node=0;node<xNodes;node++){//xNodes is the number of elements of the x vector
+                         temp+=CORE.AllCores[Xd][Yd][Cd].ValuesIn[Vp[node]];
+                }
+                        CORE.AllCores[Xd][Yd][Cd].TargetTable.Values=temp;
+                        CORE.AllCores[Xd][Yd][Cd].TargetTable.lasres=CORE.AllCores[Xd][Yd][Cd].TargetTable[Od].Value*temp;
+                        E.Value=lasres;
+                        LoadFire(Event E,Core CORE,Deal DEAL);
+        }
 }
 void Simulate::LoadFire(event E, Core CORE, Dealer DEAL){
-     unsigned Xs, Ys, Cs, Os;//when are they initialised for use??
+     unsigned Xs, Ys, Cs, Os;
      KeyTo(E.Ks, Xs, Ys, Cs, Os);
      if(E.Value==NULL){
      	E.Value = CORE.AllCores[Xs][Ys][Cs].TargetTable[Os].Value;
@@ -91,4 +99,39 @@ void Simulate::LoadFire(event E, Core CORE, Dealer DEAL){
      EventQ.push(E);//IMPORTANT
      return;
 }
+void Simulate::SimBegin(Task TASK, Dealer DEAL, Core CORE, MakeMCTables MCT) {
+   event E;
+   int l = 0;
+   unsigned counter;
+   long loops = 0;
+   unsigned Thru=0;
+   puts("Starting simulation");
+   LoadFireAll(TASK, DEAL);
+    while(true){
+       if(EventQ.empty()){
+         if(l == 40) break;
+	 if(counter==14){counter=4;}
+          UpdateAll(TASK, DEAL);
+          counter++;
+	  l++;
+        }
+        E = EventQ.front();
+        //printf("Event type = %d, %lu more events to go\n",E.Type, EventQ.size());
+        //2cout << "Event type = " << E.Type << " " << EventQ.size() << " more events to go\n";
+        EventQ.pop();
+        if(E.Type == LOADFIRE)
+            LoadFire(E, CORE, DEAL);
+        else if(E.Type == UPDATE)
+            Update(E, CORE);
+        else if(E.Type == FIREAWAY)
+            Thru += FireAway(E, MCT,CORE);
+        else {
+            printf("unknown event %d\n",E.Type);
+            exit(1);
+        }
+        loops++;
+   }
+   printf("%lu events in total\n%u Through routes\n",loops,Thru);
+}
+
 
